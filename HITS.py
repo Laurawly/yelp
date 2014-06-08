737a3ea83fd33020171845dee5e2b3e74094f253
USER <laura@ubuntu.ubuntu-domain>
2014-06-08 15:12:15 -0700

    delete original hits

--- HITS.py
+++ HITS.py
@@ -65,42 +65,6 @@ def main():
   # analysis
   u,b = hits_score(R,B)
 
-def hits(G,max_iter=100,tol=1.0e-8,nstart=None):
-    if type(G) == nx.MultiGraph or type(G) == nx.MultiDiGraph:
-        raise Exception("hits() not defined for graphs with multiedges.")
-
-    # choose fixed starting vector if not given
-    h=dict.fromkeys(G,1.0/G.number_of_nodes())
-    i=0
-    while True: # power iteration: make up to max_iter iterations
-        hlast=h
-        h=dict.fromkeys(hlast.keys(),0)
-        a=dict.fromkeys(hlast.keys(),0)
-        # this "matrix multiply" looks odd because it is
-        # doing a left multiply a^T=hlast^T*G 
-        for n in h:
-            for nbr in G[n]:
-                a[nbr]+=hlast[n]*G[n][nbr].get('weight',1)
-        # now multiply h=Ga
-        for n in h:
-            for nbr in G[n]:
-                h[n]+=a[nbr]*G[n][nbr].get('weight',1)
-        # normalize vector 
-        s=1.0/sum(h.values())
-        for n in h: h[n]*=s
-        # normalize vector 
-        s=1.0/sum(a.values())
-        for n in a: a[n]*=s
-        # check convergence, l1 norm            
-        err=sum([abs(h[n]-hlast[n]) for n in h])
-        if err < tol:
-            break
-        if i>max_iter:
-            raise NetworkXError(\
-            "HITS: power iteration failed to converge in %d iterations."%(i+1))
-        i+=1
-    return h,a
-
 
 if __name__ == '__main__':
   main()
